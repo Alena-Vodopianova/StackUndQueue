7c97574083d89890600d2b384c8a92235bb1b329
oleh-haievyi <oleh.haievyi@edu.fh-joanneum.at>
2025-05-20 01:39:01 +0200

    Added queue array and list tests to App.java

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -20,8 +20,7 @@ public class App {
         System.out.println("Pop: " + stack.pop());    // Hallo
         System.out.println("Leer? " + stack.isEmpty()); // true */
 
-
-         MyStackList<Integer> stack = new MyStackList<>();
+        MyStackList<Integer> stack = new MyStackList<>();
 
         System.out.println("Leer? " + stack.isEmpty()); // true
         stack.push(1);
@@ -36,5 +35,28 @@ public class App {
         System.out.println("Pop: " + stack.pop());    // 1
         System.out.println("Leer? " + stack.isEmpty()); // true
 
+        MyQueueArray<String> queueArray = new MyQueueArray<>();
+        System.out.println("Leer? " + queueArray.isEmpty()); // true
+        queueArray.put("A");
+        queueArray.put("B");
+        queueArray.put("C");
+        System.out.println("Groesse: " + queueArray.size()); // 3
+        System.out.println("Get: " + queueArray.get());    // A
+        System.out.println("Get: " + queueArray.get());    // B
+        System.out.println("Groesse: " + queueArray.size()); // 1
+        System.out.println("Leer? " + queueArray.isEmpty()); // false
+        System.out.println("Get: " + queueArray.get());    // C
+        System.out.println("Leer? " + queueArray.isEmpty()); // true
+
+        MyQueueList<Integer> queueList = new MyQueueList<>();
+        System.out.println("Leer? " + queueList.isEmpty()); // true
+        queueList.put(10);
+        queueList.put(20);
+        queueList.put(30);
+        System.out.println("Groesse: " + queueList.size()); // 3
+        System.out.println("Get: " + queueList.get());    // 10
+        System.out.println("Get: " + queueList.get());    // 20
+        System.out.println("Get: " + queueList.get());    // 30
+        System.out.println("Leer? " + queueList.isEmpty()); // true
     }
 }
